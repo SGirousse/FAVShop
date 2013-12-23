a4a00bb0489faa76132606bf3430cd9a55af2dc7
nightaelia <nightaelia@gmail.com>
2013-12-23 23:39:12 +0100

    add barcode
    Add barcode (for the second time, first one crach because of a merge
    ...)

--- src/com/sgf/favshop/NewFAVActivity.java
+++ src/com/sgf/favshop/NewFAVActivity.java
@@ -1,15 +1,24 @@
 package com.sgf.favshop;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.sgf.libext.ZBarConstants;
+import com.sgf.libext.ZBarScannerActivity;
 import com.sgf.listeners.SaveNewFAVButton;
 
-public class NewFAVActivity extends Activity{
+public class NewFAVActivity extends Activity implements OnClickListener{
 	
 	private SaveNewFAVButton _saveNewFAV_button;
+	private static final int ZBAR_SCANNER_REQUEST = 0;
+	private static final int ZBAR_QR_SCANNER_REQUEST = 1;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,12 +28,43 @@ public class NewFAVActivity extends Activity{
         // -- Listeners -- //
         _saveNewFAV_button = new SaveNewFAVButton(this);
         Button saveNewFAV_button = (Button) findViewById(R.id.buttonSave);
+        Button scan_button = (Button) findViewById(R.id.buttonScan);
         saveNewFAV_button.setOnClickListener(_saveNewFAV_button);
+        
+        scan_button.setOnClickListener(this);
     }
+    
+    
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+
+
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		Intent intent = new Intent(this, ZBarScannerActivity.class);
+		startActivityForResult(intent, ZBAR_SCANNER_REQUEST);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{    
+	    if (resultCode == RESULT_OK) 
+	    {
+	        // Scan result is available by making a call to data.getStringExtra(ZBarConstants.SCAN_RESULT)
+	        // Type of the scan result is available by making a call to data.getStringExtra(ZBarConstants.SCAN_RESULT_TYPE)
+	        //Toast.makeText(this, "Scan Result = " + data.getStringExtra(ZBarConstants.SCAN_RESULT), Toast.LENGTH_SHORT).show();
+	        //Toast.makeText(this, "Scan Result Type = " + data.getStringExtra(ZBarConstants.SCAN_RESULT_TYPE), Toast.LENGTH_SHORT).show();
+	        EditText barcode_box = (EditText) findViewById(R.id.editTextNewFavBarcodeContent);
+	        barcode_box.setText(data.getStringExtra(ZBarConstants.SCAN_RESULT));
+	        // The value of type indicates one of the symbols listed in Advanced Options below.
+	    } else if(resultCode == RESULT_CANCELED) {
+	        Toast.makeText(this, "Camera unavailable", Toast.LENGTH_SHORT).show();
+	    }
+	}
 }
